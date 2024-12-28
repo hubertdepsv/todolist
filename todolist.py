7e513f0a0223bff8a190f4649845199e059418a8
Hubert de Parseval <hp98@hotmail.fr>
2024-12-28 19:22:02 +0100

    Add select and each on TodoList

--- todolist.py
+++ todolist.py
@@ -79,6 +79,17 @@ class TodoList:
     def remove_at(self, index):
         self._todos.pop(index)
 
+    def each(self, callback):
+        for todo in self._todos:
+            callback(todo)
+
+    def select(self, callback):
+        new_todo_list = TodoList(self.title)
+        for todo in self._todos:
+            if callback(todo):
+                new_todo_list.add(todo)
+        return new_todo_list
+
 # test cases
 empty_todo_list = TodoList('Nothing Doing')
 
@@ -298,3 +309,50 @@ def setup():
 #     # ---- Today's Todos -----
 
 # step_10()
+
+# def step_11():
+#     print('--------------------------------- Step 11')
+#     todo_list = setup()
+
+#     todo_list.mark_all_undone()
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [ ] Buy milk
+#     # [ ] Clean room
+#     # [ ] Go to gym
+
+#     def done_if_y_in_title(todo):
+#         if 'y' in todo.title:
+#             todo.done = True
+
+#     todo_list.each(done_if_y_in_title)
+#     print(todo_list)
+#     # ---- Today's Todos -----
+#     # [X] Buy milk
+#     # [ ] Clean room
+#     # [X] Go to gym
+
+#     todo_list.each(lambda todo: print('>>>', todo))
+#     # >>> [X] Buy milk
+#     # >>> [ ] Clean room
+#     # >>> [X] Go to gym
+
+# step_11()
+
+def step_12():
+    print('--------------------------------- Step 12')
+    todo_list = setup()
+
+    def y_in_title(todo):
+        return 'y' in todo.title
+
+    print(todo_list.select(y_in_title))
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Go to gym
+
+    print(todo_list.select(lambda todo: todo.done))
+    # ---- Today's Todos -----
+    # [X] Clean room
+
+step_12()
