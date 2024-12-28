fcdf024f0348629c3f50491aa92a2894ddde0c67
Hubert de Parseval <hp98@hotmail.fr>
2024-12-28 18:41:41 +0100

    First order implementation of for_each

---
+++ for_each.py
@@ -0,0 +1,21 @@
+def for_each(callback, iterable):
+    return [callback(item) for item in iterable]
+
+for_each(lambda number: print(number**2), [1, 2, 3, 4, 5])
+# 1
+# 4
+# 9
+# 16
+# 25
+
+pets = ('cat', 'dog', 'fish', 'bearded dragon')
+for_each(lambda string: print(string.title()), pets)
+# Cat
+# Dog
+# Fish
+# Bearded Dragon
+
+nested_lists = [[1, 2], [3, 4], [5, 6, 7]]
+for_each(lambda sublist: sublist.pop(), nested_lists)
+print(nested_lists)
+# [[1], [3], [5, 6]]
